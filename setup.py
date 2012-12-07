2aa07383981538ea478f6712e9bb1ae9a9206f73
Peter Reimer <peter@4pi.org>
2012-12-07 15:57:24 +0100

    install Pillow as dependency, PIL does not work well with easy_setup

--- setup.py
+++ setup.py
@@ -3,27 +3,29 @@
 from setuptools import setup
 
 setup(name='DeepZoomTools',
-      version='1.1',
-      description='Python tools for generating Deep Zoom images (DZI) and \
+    version='1.1',
+    description='Python tools for generating Deep Zoom images (DZI) and \
 collections (DZC) for the use with Silverlight Deep Zoom, Seadragon Ajax, \
 Seadragon Mobile, OpenZoom and SaladoPlayer.',
-      author='Peter Reimer',
-      author_email='peter@4pi.org',
-      download_url='https://github.com/peterreimer/deepzoom.py/downloads',
-      keywords='deepzoom seadragon dzi dzc seadragonajax seadragonmobile silverlightdeepzoom microsoft openzoom',
-      url='http://github.com/peterreimer/deepzoom.py',
-      py_modules=['deepzoom'],
-      classifiers=['Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities',
-                   'Topic :: Multimedia :: Graphics',
-                   'Topic :: Multimedia :: Graphics :: Graphics Conversion'],
-      entry_points={
-          'console_scripts':[
-            'deepzoom=deepzoom:main'
-          ]  
-      },
-
-      )
+    author='Peter Reimer',
+    author_email='peter@4pi.org',
+    install_requires=[
+        'Pillow'
+    ],
+    download_url='https://github.com/peterreimer/deepzoom.py/downloads',
+    keywords='deepzoom seadragon dzi dzc seadragonajax seadragonmobile silverlightdeepzoom microsoft openzoom',
+    url='http://github.com/peterreimer/deepzoom.py',
+    py_modules=['deepzoom'],
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities',
+                 'Topic :: Multimedia :: Graphics',
+                 'Topic :: Multimedia :: Graphics :: Graphics Conversion'],
+    entry_points={
+        'console_scripts':[
+          'deepzoom=deepzoom:main'
+        ]  
+    },
+    )
